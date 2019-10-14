fce7e011c87f4bbd388bc88e3fbc69da65013706
EsPiRi <sezginsami@gmail.com>
2019-10-14 17:06:04 +0300

    first things first
    practicing in python

---
+++ first/first.py
@@ -0,0 +1,24 @@
+my_var=10
+my_var=my_var+3
+print(my_var)
+my_var="yellow"
+print(my_var)
+string1='i am enclosed in single quotes.' #using single quotes in string
+string2="i am enclosed in double quotes." #using double quotes in string
+print(string1+"\n"+string2)
+
+
+string3='it doesn\'t look good at all.' #using single quotes in strings but with an extra
+print(string3)
+txt="""He said:"you should get something" """
+print(txt)
+
+
+s="Hello python"  #getting the character of the string
+print(s[0])
+print(s[len(s)-1])
+print(s[-2])
+
+#concatenating strings
+string4="hello "+"world"
+print(string4)
