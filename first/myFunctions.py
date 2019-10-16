3f2c8f2163f681d7282d27d6ee2d78c0d66008dd
EsPiRi <sezginsami@gmail.com>
2019-10-17 01:45:32 +0300

    Final part of python basics
    User defined classes and creating its objects (Starting OOP), next stop in another repository

--- first/myFunctions.py
+++ first/myFunctions.py
@@ -8,3 +8,33 @@ def absolute_value(number):
         return number
     else:
         return -number
+
+
+def shutdown(yn):
+    if yn.lower()=="y":
+        return "Closing files and shutdown"
+    elif yn.lower()=="n":
+        return "Shutdown terminated"
+    else:
+        return "Check your response"
+
+
+def calculator(x,y):
+    return x*y+2
+
+
+def factorial(count):
+    if count==0:
+        return 1
+    elif count<0:
+        return "Number can NOT be less than zero"
+    else:
+        return count*factorial(count-1)
+
+
+def members_total(n):
+    return n*3
+
+
+def org_total(m):
+    return members_total(m)+5
--- first/second.py
+++ first/second.py
@@ -1,43 +1,43 @@
-#boolean data type
+# boolean data type
 
-bool1=4==2*3
-bool2=10<2*2**3
-bool3=8>2*4+1
+bool1 = 4 == 2 * 3
+bool2 = 10 < 2 * 2 ** 3
+bool3 = 8 > 2 * 4 + 1
 print(bool1)
 print(bool2)
 print(bool3)
 print("\n\n")
-#lists
-my_list=[bool1,bool2,bool3]
-colors=["red","orange","yellow","green","indigo","white"]
+# lists
+my_list = [bool1, bool2, bool3]
+colors = ["red", "orange", "yellow", "green", "indigo", "white"]
 
-print(colors[0])    #red
-print(colors[4])    #indigo
-print(len(colors))  #6
-print(colors)   #print the list of the colors
+print(colors[0])  # red
+print(colors[4])  # indigo
+print(len(colors))  # 6
+print(colors)  # print the list of the colors
 
-#designing the list for the rainbow colors
+# designing the list for the rainbow colors
 colors.remove("white")
 colors.append("violet")
 print(colors)
 
-colors.insert(4,"blue")
+colors.insert(4, "blue")
 print(colors)
 
-#slicing list
+# slicing list
 print(colors[3:6])
 
-#dictionary
-menu={"spam":12.50,"carbonara":20,"salad":15}
+# dictionary
+menu = {"spam": 12.50, "carbonara": 20, "salad": 15}
 print(len(menu))
 print(menu)
-menu["cake"]=6
+menu["cake"] = 6
 print(menu)
 
 del menu["spam"]
 print(menu)
 
-menu["carbonara"]=22
+menu["carbonara"] = 22
 print(menu)
 
 dict.clear(menu)
